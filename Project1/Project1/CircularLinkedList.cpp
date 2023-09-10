7bedf0f43f859134ec743e2345dce613e1223271
d0nghoong <slimgreg5208@gmail.com>
2023-09-10 18:43:59 +0900

    코드 정리
    코드를 세분화하여 정리 하였다

--- Project1/Project1/CircularLinkedList.cpp
+++ Project1/Project1/CircularLinkedList.cpp
@@ -2,22 +2,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-
-typedef struct ListNode {
-	char data[4];
-	struct ListNode* link;
-}listNode;
-
-typedef struct {
-	listNode* head;
-}linkedList_h;
-
-linkedList_h* createdLinkedList_h();
-void printList(linkedList_h* CL);
-void InsertFirstNode(linkedList_h* CL, char* x);
-void InsertMiddleNode(linkedList_h* CL, listNode* pre, char* x);
-void deleteNode(linkedList_h* CL, listNode* old);
-listNode* searchNode(linkedList_h* CL, char* x);
+#include"CircularLinkedList.h"
 
 linkedList_h* createdLinkedList_h() {
 	linkedList_h* CL;
@@ -107,32 +92,4 @@ listNode* searchNode(linkedList_h* CL, char* x) {
 		temp = temp->link;
 	} while (temp != CL->head);
 	return NULL;
-}
-
-void main() {
-	linkedList_h* CL;
-	listNode* p;
-
-	CL = createdLinkedList_h();
-	printf("(1) 원형리스트 생성하기\n");
-	printList(CL);
-
-	printf("\n(2) 원형 연결 리스트에 [월] 노드 삽입하기!\n");
-	InsertFirstNode(CL, "월");
-	printList(CL);
-
-	printf("\n(3) 원형 연결 리스트의 [월] 노드 뒤에 [수] 노드 삽입하기\n");
-	p = searchNode(CL, "월");
-	InsertMiddleNode(CL, p, "수");
-	printList(CL);
-
-	printf("\n(4) 원형 연결 리스트의 [수] 노드 뒤에 [금] 노드 삽입하기\n");
-	p = searchNode(CL, "수");
-	InsertMiddleNode(CL, p, "금");
-	printList(CL);
-
-	printf("\n (5) 원형 연결 리스트에서 [수] 노드 삭제하기");
-	p = searchNode(CL, "수");
-	deleteNode(CL, p);
-	printList(CL);
 }
