ad1c4724d8b6ca23fa58396915a330b5897b4830
d0nghoong <slimgreg5208@gmail.com>
2023-09-11 21:32:04 +0900

    소스 파일 분할
    가독성을 위하여 소스파일을 분할하였다

--- Project1/Project1/소스1.cpp
+++ Project1/Project1/소스1.cpp
@@ -2,22 +2,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-
-typedef struct ListNode {
-	struct ListNode* llink;
-	char data[4];
-	struct ListNode* rlink;
-}listNode;
-
-typedef struct {
-	listNode* head;
-}linkedList_h;
-
-linkedList_h* createLinkedList_h();
-void printList(linkedList_h* DL);
-void insertNode(linkedList_h* DL, listNode* pre, char* x);
-void deleteNode(linkedList_h* DL, listNode* old);
-listNode* searchNode(linkedList_h* DL, char* x);
+#include"DoubleLinkedlIst.h"
 
 linkedList_h* createdLinkedList_h() {
 	linkedList_h* DL;
@@ -84,35 +69,4 @@ listNode* searchNode(linkedList_h* DL, char* x) {
 		else temp = temp->rlink;
 	}
 	return temp;
-}
-
-void main() {
-	linkedList_h* DL;
-	listNode* p;
-
-	printf("(1) 이중 연결 리스트 생성하기\n");
-	DL = createdLinkedList_h();
-	printList(DL);
-
-	printf("\n(2) 이중 연결 리스트에 [월] 노드 삽입하기\n");
-	insertNode(DL, NULL, "월");
-	printList(DL);
-
-	printf("\n(3) 이중 연결 리스트의 [월] 노드 뒤에 [수] 노드 삽입하기\n");
-	p = searchNode(DL, "월");
-	insertNode(DL, p, "수");
-	printList(DL);
-
-	printf("\n(4) 이중 연결 리스트의 [수] 노드 뒤에 [금] 노드 삽입하기\n");
-	p = searchNode(DL, "수");
-	insertNode(DL, p, "금");
-	printList(DL);
-
-	printf("\n(5) 이중 연결 리스트에서 [수] 노드 삭제하기\n");
-	p = searchNode(DL, "수");
-	deleteNode(DL, p);
-	printList(DL);
-
-
-
 }
